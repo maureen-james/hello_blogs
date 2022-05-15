8ec4ee58edcf17482b90ecfb9dda112e7c424b2d
maureen-james <manjax97@gmail.com>
2022-05-15 14:19:01 +0300

    add blogs route

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,15 @@
 from app import main
 from flask import render_template,request,redirect,url_for,abort
+from ..models import Blog
 
 
 
 
+# @main.route('/')
+# def index():
+#     return render_template('index.html')
 @main.route('/')
 def index():
-    return render_template('index.html')
+    blog=Blog.query.all()
+
+    return render_template('index.html', blog=blog)    
