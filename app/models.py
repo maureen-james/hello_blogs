3094f1df7bc7c3d40a801962e1b6dfbd13b6f25d
maureen-james <manjax97@gmail.com>
2022-05-15 14:25:46 +0300

    add comment class in models

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(255), unique=True, index=True)
     pass_secure = db.Column(db.String(255))
     bio = db.Column(db.String(255))
-    pitches=db.relationship('Pitch',backref='user',lazy='dynamic')
+    blog=db.relationship('Blog',backref='user',lazy='dynamic')
     comment = db.relationship('Comment', backref='user', lazy='dynamic')
     like = db.relationship('Likes', backref='user', lazy='dynamic')
     dislikes = db.relationship('Dislikes', backref='user', lazy='dynamic')
@@ -39,16 +39,16 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
-class Pitch(db.Model):
-    __tablename__ = 'pitches'
+class Blog(db.Model):
+    __tablename__ = 'blog'
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(255), nullable = False)
     post = db.Column(db.Text(), nullable = False)
-    comment = db.relationship('Comment', backref='pitch', lazy='dynamic')
+    comment = db.relationship('Comment', backref='blog', lazy='dynamic')
     category = db.Column(db.String(255), index = True,nullable = False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    like = db.relationship('Likes', backref='pitch', lazy='dynamic')
-    dislikes = db.relationship('Dislikes', backref='pitch', lazy='dynamic')
+    like = db.relationship('Likes', backref='blog', lazy='dynamic')
+    dislikes = db.relationship('Dislikes', backref='blog', lazy='dynamic')
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def save(self):
@@ -60,14 +60,14 @@ class Pitch(db.Model):
         db.session.commit()
 
     def __repr__(self):
-        return f"Pitch {self.title}"
+        return f"Blog {self.title}"
 
 
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'), nullable=False)
+    pitch_id = db.Column(db.Integer, db.ForeignKey('blog.id'), nullable=False)
     comment = db.Column(db.Text(),nullable = False) 
     
     def save_c(self):
@@ -75,8 +75,8 @@ class Comment(db.Model):
         db.session.commit()
 
     @classmethod
-    def get_comments(cls, pitch_id):
-        comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+    def get_comments(cls, blog_id):
+        comments = Comment.query.filter_by(blog_id=blog_id).all()
         return comments
 
     def delete(self):
